96650be97065d74d4590cc447350b85a066b2b0f
Spencer Lyons <sjl8r8@gmail.com>
2020-01-21 15:33:49 -0600

    didn't complete bonus yet...going to need some help with this one

--- src/main/BonusBinarySearch.java
+++ src/main/BonusBinarySearch.java
@@ -11,7 +11,7 @@ public class BonusBinarySearch {
      * @param n - number to search for
      * @return index of search item if it's found, -1 if not found
      */
-    public static int binarySearch(int[] sortedNumbers, int n) {
+    public static int binarySearch(int [] sortedNumbers, int n) {
         int right = sortedNumbers.length - 1;
         int left = 0;
         while (right >= left) {
