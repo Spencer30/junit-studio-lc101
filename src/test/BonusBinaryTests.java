59c0cb1b8cba13f5645c1ff598fdf27841e9d49d
Spencer Lyons <sjl8r8@gmail.com>
2020-01-22 18:47:05 -0600

    still working on it

---
+++ src/test/BonusBinaryTests.java
@@ -0,0 +1,21 @@
+package test;
+
+import main.BonusBinarySearch;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BonusBinaryTests {
+    //TODO: add tests here
+    @Test
+    public void emptyTest() {
+        assertEquals(true, true);
+    }
+
+//    @Test
+//    public void newTest() {
+//        int returnValue = BonusBinarySearch.binarySearch([1,2,3], 2);
+//        System.out.println(returnValue);
+//    }
+
+}
